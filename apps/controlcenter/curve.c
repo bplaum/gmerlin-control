5dd39c3156774e30cd9a425bc17a2d3a1d6f6ec1
Burkhard Plaum <burkhardplaum@gmail.com>
2024-05-28 00:17:42 +0200

    Forgot this file

---
+++ apps/controlcenter/curve.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+#include "controlcenter.h"
+
+
+curve_t * curve_create(const char * path, gavl_time_t length, bg_msg_sink_t * sink)
+  {
+  curve_t * c = calloc(1, sizeof(*c));
+
+  
+  
+  return c;
+  }
+
+void curve_append(curve_t * h, gavl_array_t * arr, const gavl_value_t * val)
+  {
+  
+  }
